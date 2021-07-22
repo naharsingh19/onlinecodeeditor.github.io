e5629633009adff3dc69b5090055c6efba36aec1
nahartewatia19 <nahartewatia19@gmail.com>
2021-07-22 13:28:53 +0530

    Final Project

---
+++ src/script.js
@@ -0,0 +1,22 @@
+function run () {
+    var htmlCode = $("#html-container").val();
+    var cssCode = "<style>" + $("#css-container").val() + "</style>";
+    var jsCode = $("#js-container").val();
+    var result = $("#result-container");
+    result.contents()[0].body.innerHTML = htmlCode + cssCode;
+    result.prop("contentWindow").eval(jsCode);
+}
+
+$("#html-container").on("keypress change paste", run);
+$("#css-container").on("keypress change paste", run);
+$("#js-container").on("keypress change paste", run);
+
+$(".btn").click(function () {
+    $(this).toggleClass("selected");
+    var containerId = "#" + this.id + "-container";
+    $(containerId).toggleClass("hidden");
+    $(containerId).toggleClass("nh");
+    $(".panel").height($(window).height() - $("#container").height() - 15);
+})
+
+$(".panel").height($(window).height() - $("#container").height() - 15);
